3a04d2e45d1f4b5859c81c40a82302dbca658831
Puggy Potato <qingyaurou@gmail.com>
2025-06-05 23:49:25 +0800

    added a small block toDo:Make The Block Move

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,10 +2,35 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  function moveUp(){
+
+  }
+
+  function moveDown(){
+
+  }
+
+  function moveLeft(){
+
+  }
+
+  function moveRight(){
+
+  }
+
   return (
     <>
-      <div>
-        
+      <div id='container'>
+        <div id='character'>
+
+        </div>
+        <div id='aboutMe'>
+          <h1 id='name'>Tang Qing Yau</h1>
+          <h3 id='title'>Full-Stack Developer / Web3 Developer</h3>
+          <p id='bio'>Bored.</p>
+          <a href='/journey'>Check My Journey</a>
+        </div>
       </div>
     </>
   )
